cee66346e61088721083549a83a868ba8232d3ec
g23988 <g23988@gmail.com>
2023-12-14 00:47:49 +0800

    AJAX test

--- src/pages/sportsman/court_list/CourtList.tsx
+++ src/pages/sportsman/court_list/CourtList.tsx
@@ -6,31 +6,65 @@ import "./court_list.scss";
 
 const CourtList = () => {
     const [courtCards, setCourtCards] = useState<Array<JSX.Element>>([]);
+    const [userLocation, setUserLocation] = useState({ lat: null, lng: null });
+
+    // Function to get user's current location
+    const getLocation = () => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setUserLocation({
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    });
+                },
+                (error) => {
+                    console.error("Error Code = " + error.code + " - " + error.message);
+                }
+            );
+        } else {
+            console.log("Geolocation is not supported by this browser.");
+        }
+    };
+
+    // Function to fetch distance for a court
+    const fetchDistance = async (court) => {
+        try {
+            const response = await axios.get(`https://admin.chillmonkey.tw/v1/spaces/${court.id}/distance?lat=${userLocation.lat}&lng=${userLocation.lng}`);
+            return response.data.distance; // Extracting distance from the response
+        } catch (error) {
+            console.error('Error fetching distance:', error);
+            return null; // Return null or a default value in case of error
+        }
+    };
 
     useEffect(() => {
-        axios.get(`${BACK_END}/${VERSION}/spaces`)
-            .then((res) => {
-                const courtCards = [];
-                const courts = res.data;
-                const n = courts.length;
-                for (let i = 0; i < n; i++) {
-                    const court = courts[i];
-                    courtCards.push(
-                        <CourtCard
-                            key={i}
-                            numUsers={1}
-                            capacity={court.capacity}
-                            ballTypeStr={court.ball_type.cht_game_name}
-                            courtName={court.name}
-                            distance={300}
-                        />
-                    );                    
-                }
-                setCourtCards(courtCards);
-            }).catch((err) => {
-                console.log(err);
-            });
-    }, []);
+        getLocation(); // Get user's current location
+
+        if (userLocation.lat !== null && userLocation.lng !== null) {
+            axios.get(`${BACK_END}/${VERSION}/spaces`)
+                .then(async (res) => {
+                    const courtCards = [];
+                    const courts = res.data;
+                    for (let court of courts) {
+                        const distance = await fetchDistance(court);
+                        courtCards.push(
+                            <CourtCard
+                                key={court.id}
+                                numUsers={1}
+                                capacity={court.capacity}
+                                ballTypeStr={court.ball_type.cht_game_name}
+                                courtName={court.name}
+                                distance={distance || 'N/A'} // Use fetched distance or display 'N/A' if not available
+                            />
+                        );                    
+                    }
+                    setCourtCards(courtCards);
+                }).catch((err) => {
+                    console.log(err);
+                });
+        }
+    }, [userLocation]); // Add userLocation as a dependency
 
     return (
         <div className="sportsman-court-list">
