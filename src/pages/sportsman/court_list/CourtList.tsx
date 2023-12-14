3eb1e36d84ebb0cd8bd552f38700ea5cd3ff68fb
eugenechen-ourtimes <eugene.chen19971108@gmail.com>
2023-12-14 11:30:31 +0800

    try getCurrentPosition 2

--- src/pages/sportsman/court_list/CourtList.tsx
+++ src/pages/sportsman/court_list/CourtList.tsx
@@ -4,54 +4,37 @@ import { BACK_END, VERSION } from "../../../config";
 import CourtCard from "../../../components/court_card/CourtCard";
 import "./court_list.scss";
 
-interface UserLocation {
-    lat: number | null;
-    lng: number | null;
-}
+const fetchDistance = async (
+    courtId: number,
+    lat: number,
+    lng: number
+): Promise<number | null> => {
+    try {
+        const response = await axios.get(`https://admin.chillmonkey.tw/v1/spaces/${courtId}/distance?lat=${lat}&lng=${lng}`);
+        return response.data.distance;
+    } catch (error) {
+        console.log("fetchDistance", error);
+        return null;
+    }
+};
 
 const CourtList = () => {
     const [courtCards, setCourtCards] = useState<Array<JSX.Element>>([]);
-    const [userLocation, setUserLocation] = useState<UserLocation>({ lat: null, lng: null });
-
-    const getLocation = () => {
-        if (window.navigator.geolocation) {
-            window.navigator.geolocation.getCurrentPosition(
-                (position) => {
-                    setUserLocation({
-                        lat: position.coords.latitude,
-                        lng: position.coords.longitude
-                    });
-
-                    console.log("getLocation", position.coords.latitude, position.coords.longitude);
-                },
-                (error) => {
-                    console.log("getLocation", error);
-                }
-            );
-        }
-    };
-
-    const fetchDistance = async (court: any): Promise<number | null> => {
-        try {
-            const response = await axios.get(`https://admin.chillmonkey.tw/v1/spaces/${court.id}/distance?lat=${userLocation.lat}&lng=${userLocation.lng}`);
-            return response.data.distance;
-        } catch (error) {
-            console.log("fetchDistance", error);
-            return null;
-        }
-    };
-
-    useEffect(() => {
-        getLocation();
+    const fetchCourtsAndUpdateDivs = (position: GeolocationPosition | null) => {
         axios.get(`${BACK_END}/${VERSION}/spaces`)
             .then(async (res) => {
-                const courtCards = [];
+                const newCourtCards = [];
                 const courts = res.data;
                 const n = courts.length;
                 for (let i = 0; i < n; i++) {
                     const court = courts[i];
-                    const distance = await fetchDistance(court);
-                    courtCards.push(
+                    const distance = position === null ? null : await fetchDistance(
+                        court.id,
+                        position.coords.latitude,
+                        position.coords.longitude
+                    );
+
+                    newCourtCards.push(
                         <CourtCard
                             key={i}
                             numUsers={1}
@@ -60,12 +43,28 @@ const CourtList = () => {
                             courtName={court.name}
                             distance={distance}
                         />
-                    );                    
+                    );
                 }
-                setCourtCards(courtCards);
+
+                setCourtCards(newCourtCards);
             }).catch((err) => {
-                console.log(err);
+                console.log("fetchCourtsAndUpdateDivs", err);
             });
+    };
+
+    useEffect(() => {
+        if (window.navigator.geolocation) {
+            window.navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    fetchCourtsAndUpdateDivs(position);
+                },
+                (error) => {
+                    console.log("getCurrentPosition", error);
+                    fetchCourtsAndUpdateDivs(null);
+                });
+        } else {
+            fetchCourtsAndUpdateDivs(null);
+        }
     }, []);
 
     return (
